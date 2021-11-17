b729fe93fb8874758317e9019075e7b75f312d7b
Stephen Kurtis <stephen7347@gmail.com>
2021-11-17 15:20:41 -0500

    Test Push, Added comment to HelloApplication.java

--- src/main/java/com/shc/serverhealthchecker/HelloApplication.java
+++ src/main/java/com/shc/serverhealthchecker/HelloApplication.java
@@ -17,7 +17,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-
+        //Test Commit Stephen Kurtis
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
