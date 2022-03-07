0da9973cff953df628d1db3473eead813cb09fa9
DaiyaoJ <93630214+DaiyaoJ@users.noreply.github.com>
2022-03-07 02:08:20 -0500

    Update HelloController.java

--- src/main/java/com/shc/serverhealthchecker/HelloController.java
+++ src/main/java/com/shc/serverhealthchecker/HelloController.java
@@ -1,14 +1,44 @@
 package com.shc.serverhealthchecker;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private Stage stage;
+    private Scene scene;
+    
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void switchScene1(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("scene1.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Main Menu");
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    protected void switchScene2(ActionEvent event) throws IOException { //second user interface
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("scene2.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Select tests");
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    protected void switchScene3(ActionEvent event) throws IOException{ //third user interface
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("scene3.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Results");
+        stage.setScene(scene);
+        stage.show();
     }
 }
