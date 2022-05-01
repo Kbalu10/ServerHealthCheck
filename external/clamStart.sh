8f4fa168d24cc844334c9e81fb5ed8599ee713cd
Kbalu10 <kbalu0000@gmail.com>
2022-05-01 18:04:22 -0400

    VirusCheck Bug Fixes + .sh Fix

--- external/clamStart.sh
+++ external/clamStart.sh
@@ -1,5 +1,6 @@
 sudo apt install ./clamav-0.104.2.linux.x86_64.deb -y
 sudo groupadd clamav
 sudo useradd -g clamav -s /bin/false -c "Clam Antivirus" clamav
-sudo chown -R clamav:clamav /usr/local/share/clamav
 sudo cp freshclam.conf /usr/local/etc/freshclam.conf
+sudo freshclam
+sudo chown -R clamav:clamav /usr/local/share/clamav
