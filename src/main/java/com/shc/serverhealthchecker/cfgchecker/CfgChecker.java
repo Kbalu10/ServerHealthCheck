3e3a1c8a8d3174c0ec8ad7a585f2c3622a5411c4
Stephen <stephen7347@gmail.com>
2022-05-02 10:44:40 -0400

    CFG Checker checks for valid file path and can now determine when the last time a file was modified. Gives a message if a file has been modified within the last 24 hours.

--- src/main/java/com/shc/serverhealthchecker/cfgchecker/CfgChecker.java
+++ src/main/java/com/shc/serverhealthchecker/cfgchecker/CfgChecker.java
@@ -7,11 +7,13 @@ import javafx.scene.control.TextArea;
 
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileOwnerAttributeView;
 import java.nio.file.attribute.UserPrincipal;
 
@@ -37,26 +39,52 @@ public class CfgChecker extends Checker {
                 this.proc = pr;
                 BufferedReader reader = new BufferedReader(new FileReader("external/CFGPaths"));
                 String line = reader.readLine();
+
+                //parses CFGPaths file which contains important CFG file paths
                 while (line != null) {
                     Path path = Paths.get(line);
-                    FileOwnerAttributeView file = Files.getFileAttributeView(path, FileOwnerAttributeView.class);
-                    try {
-                        UserPrincipal user = file.getOwner();
-                        String ownerMsg = "File Path: " + path + " | Owner: " + user.getName();
-                        Msg ownerName = new Msg(1, "File Owner", ownerMsg, "CFG Checker");
-                        this.controller.reportMsg(ownerName);
-                        //System.out.println("File Path: " + path + " | Owner: " + user.getName());
-                        if (user.getName().equals("root")) {
-                            //System.out.println("Warning: " + "\"" + path + "\" " + "Has Root Access!");
-                            String hasRoot = "Warning: " + "\"" + path + "\" " + "Has Root Access!";
-                            Msg rootMsg = new Msg(1, "Root Access", hasRoot, "CFG Checker" );
-                            this.controller.reportMsg(rootMsg);
+
+                    //checks if the file path exists
+                    if(Files.exists(path)) {
+                        FileOwnerAttributeView file = Files.getFileAttributeView(path, FileOwnerAttributeView.class);
+                        try {
+                            //Checks the owner from the CFGPaths list of CFG files and gives a warning if the owner is root
+                            UserPrincipal user = file.getOwner();
+                            String ownerMsg = "File Path: " + path + " | Owner: " + user.getName();
+                            Msg ownerName = new Msg(1, "File Owner", ownerMsg, "CFG Checker");
+                            this.controller.reportMsg(ownerName);
+                            //System.out.println("File Path: " + path + " | Owner: " + user.getName());
+                            if (user.getName().equals("root")) {
+                                //System.out.println("Warning: " + "\"" + path + "\" " + "Has Root Access!");
+                                String hasRoot = "Warning: " + "\"" + path + "\" " + "Has Root Access!";
+                                Msg rootMsg = new Msg(1, "Root Access", hasRoot, "CFG Checker");
+                                this.controller.reportMsg(rootMsg);
+                            }
+
+                            //Determines when the current file was last modified and warns if it is within 24 hours
+                            Path detailedfilet = Paths.get(String.valueOf(path));
+                            BasicFileAttributes attr = Files.readAttributes(detailedfilet, BasicFileAttributes.class);
+                            String filename = String.valueOf(path);
+                            File filet = new File(filename);
+                            long lastmodded = filet.lastModified();
+                            String modString = path + " has been modified within the last 24 hours. Last Modified: " + attr.lastModifiedTime();
+                            if (lastmodded > System.currentTimeMillis() - 86400000) {
+                                Msg modMsg = new Msg(1, "File Modified", modString, "CFG Checker");
+                                this.controller.reportMsg(modMsg);
+                            }
+                        } catch (Exception e) {
+                            System.out.println(e);
                         }
-                    } catch (Exception e) {
-                        System.out.println(e);
                     }
-                    line = reader.readLine();
-                }
+                    //Reports message that file path is invalid
+                    else{
+                        System.out.println("BAD PATHBAD PATHBAD PATHBAD PATHBAD PATHBAD PATHBAD PATHBAD PATHBAD PATHBAD PATHBAD PATHBAD PATHBAD PATH");
+                        String invalidPath = path + " Does not exist";
+                        Msg noPath = new Msg(1, "Invalid File Path", invalidPath, "CFG Checker");
+                    }
+                        line = reader.readLine();
+                    }
+
                 reader.close();
             } catch (IOException e) {
                 e.printStackTrace();
