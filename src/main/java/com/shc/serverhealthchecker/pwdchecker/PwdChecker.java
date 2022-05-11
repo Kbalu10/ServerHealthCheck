df8e78aacd31be1d552942830a91f61786e0ff9f
Stephen <stephen7347@gmail.com>
2022-05-11 19:38:51 -0400

    Implementation of password checker

--- src/main/java/com/shc/serverhealthchecker/pwdchecker/PwdChecker.java
+++ src/main/java/com/shc/serverhealthchecker/pwdchecker/PwdChecker.java
@@ -4,8 +4,7 @@ import com.shc.serverhealthchecker.model.Checker;
 import com.shc.serverhealthchecker.model.Msg;
 import com.shc.serverhealthchecker.model.SHCController;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class PwdChecker extends Checker{
 
@@ -23,23 +22,57 @@ public class PwdChecker extends Checker{
         Runnable runnable = () ->
         {
             try {
-                Process pr = Runtime.getRuntime().exec("ls");
+                //Process pr = Runtime.getRuntime().exec("john --wordlist=/home/u/Downloads/rockyou.txt /home/u/Downloads/mypasswd");
+                Process pr = Runtime.getRuntime().exec("unshadow /etc/passwd /etc/shadow");
                 this.proc = pr;
-                Msg test1 = new Msg(1, "PWD CHECKER TEST PRINT", "PWD MSG TEST", "PWD Checker");
-                this.controller.reportMsg(test1);
+
 
                 BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream()));
                 String line;
+                File file = new File("unshadowed.txt");
+                FileWriter fw = new FileWriter(file);
+                PrintWriter pw = new PrintWriter(fw);
+
                 while ((line = in.readLine()) != null && !stop) {
-                    System.out.println(line);
+                    //System.out.println(line);
+                    pw.println(line);
                     myself.progress++; //
+                }
+                pw.close();
+                proc.waitFor();
+
+
+                Process pr2 = Runtime.getRuntime().exec("john --wordlist=/home/u/Downloads/rockyou.txt unshadowed.txt");
+                this.proc = pr2;
+                BufferedReader in2 = new BufferedReader(new InputStreamReader(pr2.getInputStream()));
+                while ((line = in2.readLine()) != null && !stop) {
+                    //System.out.println(line);
                     if(line.indexOf("ERROR")>=0){ //
-                        this.controller.reportMsg(new Msg(Msg.ERROR, "state", line, "PwdChecker"));
+                        this.controller.reportMsg(new Msg(Msg.ERROR, "Cracking", line, "PDWChecker"));
                     }else{
-                        this.controller.reportMsg(new Msg(Msg.WARN, "state", line, "PwdChecker"));
+                        this.controller.reportMsg(new Msg(Msg.WARN, "Cracking", line, "PWDChecker"));
                     }
                 }
-                pr.waitFor();
+                proc.waitFor();
+
+                Process pr3 = Runtime.getRuntime().exec("john --show unshadowed.txt");
+                this.proc = pr3;
+                BufferedReader in3 = new BufferedReader(new InputStreamReader(pr3.getInputStream()));
+                while ((line = in3.readLine()) != null && !stop) {
+                    //System.out.println(line);
+                    if(!line.isEmpty()){
+                        if(line.indexOf("ERROR")>=0){ //
+                            this.controller.reportMsg(new Msg(Msg.ERROR, "Cracked Password", line, "PWDChecker"));
+                        }else{
+                            this.controller.reportMsg(new Msg(Msg.WARN, "Cracked Password", line, "PWDChecker"));
+                        }
+                    }
+
+
+                }
+                proc.waitFor();
+
+
                 myself.progress = 100; //
                 System.out.println("ok!");
 
